Add rotation and contrast to classes. Fix issue in img. Better var names.
Fix issue in (img) looking different when plotting after optimisation
by adding copy(img) to start of __init__ in reconstructionSSAM class.
Changed name of some variables in reconstructResp to be more pythonic
(change from camelCase to snake_case, and understandable names).

reconstructResp.py
@@ -59,8 +59,8 @@ def getInputs():
   )
   parser.add_argument(
     "--load_data_only",
-    default="False",  #'3948',
-    type=strtobool,  # , required=True,
+    default="False",
+    type=strtobool,
     help="only load input data. Do not perform optimisation.",
   )
   parser.add_argument(
@@ -218,6 +218,14 @@ def getInputs():
     type=strtobool,
     help="Turn off print checks",
   )
+  parser.add_argument(
+    "-b",
+    "--bayesian_opt",
+    default=str(False),
+    type=strtobool,
+    help="Perform bayesian optimisation and read coeffs from file",
+  )
+
 
   args = parser.parse_args()
   return args
@@ -506,11 +514,12 @@ def matchesFromRegex(path):
 
 
 if __name__ == "__main__":
+  args = getInputs()
   date_today = str(date.today())
-  print(__doc__)
+  if not args.quiet:
+    print(__doc__)
   startTime = time()
 
-  args = getInputs()
   landmarkDir = args.inp
   case = args.case
   tag = args.out
@@ -521,9 +530,18 @@ if __name__ == "__main__":
   surfDir = args.meshdir
   numEpochs = args.epochs
   xrayEdgeFile = args.xray
-  c_edge = args.c_edge
-  c_dense = args.c_dense
-  c_prior = args.c_prior
+  if args.bayesian_opt:
+    bayes_pts = np.loadtxt("points_to_sample.txt")
+    c_dense = bayes_pts[0]
+    c_edge = bayes_pts[1]
+    c_prior = bayes_pts[2]
+  else:
+    c_dense = args.c_dense
+    c_edge = args.c_edge
+    c_prior = args.c_prior
+  print(f"c_dense is {c_dense}")
+  print(f"c_edge is {c_edge}")
+  print(f"c_prior is {c_prior}")
   c_anatomical = args.c_anatomical
   c_grad = args.c_grad
   kernel_radius = args.kernel_radius
@@ -566,7 +584,7 @@ if __name__ == "__main__":
   spacingDirs = filesFromRegex(config["luna16paths"]["spacing"])
   imDirs = filesFromRegex(config["luna16paths"]["drrs"])
   imDirs_left = filesFromRegex(config["luna16paths"]["drrs_left"])
-  # imDirs_right = filesFromRegex(config["luna16paths"]["drrs_right"])
+  imDirs_right = filesFromRegex(config["luna16paths"]["drrs_right"])
   imDirs_45 = filesFromRegex(config["luna16paths"]["drrs_-45"])
   patientIDs = matchesFromRegex(config["luna16paths"]["origins"])
 
@@ -582,6 +600,7 @@ if __name__ == "__main__":
     == len(imDirs)
     == len(originDirs)
     == len(imDirs_left)
+    == len(imDirs_right)
     == len(imDirs_45)
   ), (
     "Error reading image data. "
@@ -589,6 +608,7 @@ if __name__ == "__main__":
     f"Num imDirs dirs = {len(imDirs)}. "
     f"Num originDirs dirs = {len(originDirs)}. "
     f"Num imDirs_left dirs = {len(imDirs_left)}. "
+    f"Num imDirs_right dirs = {len(imDirs_right)}. "
     f"Num imDirs_45 dirs = {len(imDirs_45)}. "
   )
 
@@ -624,6 +644,7 @@ if __name__ == "__main__":
     spacingDirs.pop(dId)
     imDirs.pop(dId)
     imDirs_left.pop(dId)
+    imDirs_right.pop(dId)
     imDirs_45.pop(dId)
     patientIDs.pop(dId)
   missing = []
@@ -697,27 +718,39 @@ if __name__ == "__main__":
     2,
     0,
   )
-  drrArr_left = np.rollaxis(
-    # np.dstack([utils.loadXR(o)[:-2,:-2][::imgSpaceCoeff,::imgSpaceCoeff]
-    np.dstack(
-      [utils.loadXR(o)[::imgSpaceCoeff, ::imgSpaceCoeff] for o in imDirs_left]
-    ),
-    2,
-    0,
-  )
-  drrArr_45 = np.rollaxis(
-    # np.dstack([utils.loadXR(o)[:-2,:-2][::imgSpaceCoeff,::imgSpaceCoeff]
-    np.dstack(
-      [utils.loadXR(o)[::imgSpaceCoeff, ::imgSpaceCoeff] for o in imDirs_45]
-    ),
-    2,
-    0,
-  )
+  if "drrs_left" in config["training"]["img_keys"]:
+    drrArr_left = np.rollaxis(
+      # np.dstack([utils.loadXR(o)[:-2,:-2][::imgSpaceCoeff,::imgSpaceCoeff]
+      np.dstack(
+        [utils.loadXR(o)[::imgSpaceCoeff, ::imgSpaceCoeff] for o in imDirs_left]
+      ),
+      2,
+      0,
+    )
+  if "drrs_right" in config["training"]["img_keys"]:
+    drrArr_right = np.rollaxis(
+      # np.dstack([utils.loadXR(o)[:-2,:-2][::imgSpaceCoeff,::imgSpaceCoeff]
+      np.dstack(
+        [utils.loadXR(o)[::imgSpaceCoeff, ::imgSpaceCoeff] for o in imDirs_right]
+      ),
+      2,
+      0,
+    )
+  if "drrs_-45" in config["training"]["img_keys"]:
+    drrArr_45 = np.rollaxis(
+      # np.dstack([utils.loadXR(o)[:-2,:-2][::imgSpaceCoeff,::imgSpaceCoeff]
+      np.dstack(
+        [utils.loadXR(o)[::imgSpaceCoeff, ::imgSpaceCoeff] for o in imDirs_45]
+      ),
+      2,
+      0,
+    )
+
   if config["training"]["num_imgs"] == 2:
     # join so array is shape Npatients, Nimages, Npixels_x, Npixels_y
     drrArr = np.stack((drrArr, drrArr_left), axis=1)
   elif config["training"]["num_imgs"] == 3:
-    drrArr = np.stack((drrArr, drrArr_left, drrArr_45), axis=1)
+    drrArr = np.stack((drrArr, drrArr_left, drrArr_right), axis=1)
 
   # offset centered coordinates to same reference frame as CT data
   carinaArr = nodalCoordsOrig[:, 1]
@@ -770,7 +803,7 @@ if __name__ == "__main__":
   testSet.sort()
   lmProjDef = lmProj.copy()
   landmarksDef = landmarks.copy()
-  lmProj_test = []
+  ground_truth_landmarks = []
   for t in testSet[::-1]:
     # store test data in different list
     testID.append(patientIDs[t])
@@ -778,7 +811,7 @@ if __name__ == "__main__":
     testSpacing.append(spacing[t])
     testIm.append(drrArr[t])
     testLM.append(copy(landmarks[t]))
-    lmProj_test.append(copy(landmarks[t]))
+    ground_truth_landmarks.append(copy(landmarks[t]))
 
     """ """
     # remove test data from train data
@@ -824,7 +857,7 @@ if __name__ == "__main__":
   numModes = np.where(
     np.cumsum(ssam.pca_sg.explained_variance_ratio_) > describedVariance
   )[0][0]
-  print("modes used is", numModes)
+  if not args.quiet: print("modes used is", numModes)
 
   # center the lobes vertically
   # keep vertical alignment term for later use
@@ -931,7 +964,7 @@ if __name__ == "__main__":
   # reorder unstructured stl file to be coherent w/ model and landmarks
   # extract mesh data (coords, normals and faces)
   for key in shapes:
-    print(f"loading {key} mesh")
+    if not args.quiet: print(f"loading {key} mesh")
     if not args.quiet:
       print("original num cells", len(mean_mesh[key].faces()))
     if key == "Airway":
@@ -943,6 +976,7 @@ if __name__ == "__main__":
     if not args.quiet:
       print("decimated num cells", len(mesh.faces()))
     mesh_45 = mesh.clone().rotateZ(45)
+    meanNorms_face_rot45[key] = mesh_45.normals(cells=True)
     # vp = v.Plotter()
     # vp += mesh_45.alpha(0.8)
     # vp += mesh.alpha(0.2)
@@ -951,7 +985,6 @@ if __name__ == "__main__":
     # load mesh data and create silhouette
     surfCoords = mesh.points()
     meanNorms_face[key] = mesh.normals(cells=True)
-    meanNorms_face_rot45[key] = mesh_45.normals(cells=True)
     faces[key] = np.array(mesh.faces())
 
     # offset to ensure shapes are aligned to carina
@@ -968,30 +1001,38 @@ if __name__ == "__main__":
 
   tagBase = copy(tag)
   save_initial_coords = copy(inputCoords)
-  for t, (tID, tLM, tImg, tOrig, tSpace) in enumerate(
-    zip(testID, testLM, testIm, testOrigin, testSpacing)
-  ):
+  for t, (
+    target_id,
+    target_lm,
+    target_img,
+    target_origin,
+    target_spacing,
+  ) in enumerate(zip(testID, testLM, testIm, testOrigin, testSpacing)):
 
-    tag = tagBase + "_case" + tID
+    tag = tagBase + "_case" + target_id
     # load image directly from training data
-    img = tImg.copy()
+    img = target_img.copy()
 
     # index 0 as output is stacked
-    imgCoords = ssam.sam.drrArrToRealWorld(img, np.zeros(3), tSpace)[0]
-    spacing_xr = tSpace.copy()
+    imgCoords = ssam.sam.drrArrToRealWorld(img, np.zeros(3), target_spacing)[0]
+    spacing_xr = target_spacing.copy()
     # center image coords, so in the same coord system as edges
     imgCoords -= np.mean(imgCoords, axis=0)
 
     # edge points in units of pixels from edge map
     edgePoints = [None] * len(config["test-set"]["outlines"])
     for f, file in enumerate(config["test-set"]["outlines"]):
-      file_re = config["test-set"]["outlines"][f].format(tID, tID)
+      file_re = config["test-set"]["outlines"][f].format(target_id, target_id)
       print(file_re)
       edgePoints[f] = np.loadtxt(file_re, delimiter=",")
       edgePoints[f] = np.unique(edgePoints[f], axis=0)
     # if only 1 x-ray given, change shape from list of 2D arrays to one 2D array
     if len(edgePoints) == 1:
       edgePoints = edgePoints[f]
+    edgePoints_contrast = np.loadtxt(
+      config["test-set"]["contrast-outline"][0].format(target_id, target_id), 
+      delimiter=","
+    )
 
     # debug checks to show initial alignment of image with edge map,
     # and mean shape with edge map
@@ -1034,45 +1075,80 @@ if __name__ == "__main__":
               c="black",
             )
           else:
+            coords_rot = utils.rotate_coords_about_z(meanArr, config["training"]["rotation"][i])
+            
+            ax[1].scatter(edgePoints[i][:, 0], edgePoints[i][:, 1], s=2)
             ax[0].scatter(
-              utils.rotate_coords_about_z(meanArr, 45)[
-                :, config["training"]["img_axes"][i][0]
-              ],
-              meanArr[:, config["training"]["img_axes"][i][1]],
+              coords_rot[:, config["training"]["img_axes"][i][0]],
+              coords_rot[:, config["training"]["img_axes"][i][1]],
               s=1,
               c="yellow",
               alpha=0.2,
             )
+            ax[1].scatter(edgePoints[i][:, 0], edgePoints[i][:, 1], s=2)
           plt.show()
 
+    # exit()
     # declare posterior shape model class
-    assam = RespiratoryReconstructSSAM(
-      shape=inputCoords,
-      xRay=edgePoints,
-      lmOrder=lmOrder,
-      normals=None,
-      transform=carinaArr[t] * 0.0,
-      img=img,
-      # imgSpacing=spacing_xr,
-      imgCoords=imgCoords,
-      imgCoords_all=ssam.sam.imgCoords_all,
-      imgCoords_axes=config["training"]["img_axes"],
-      density=density,
-      model=modelDict,
-      modeNum=numModes,
-      c_edge=c_edge,
-      c_prior=c_prior,
-      c_dense=c_dense,
-      c_grad=c_grad,
-      c_anatomical=c_anatomical,
-      kernel_distance=kernel_distance,
-      kernel_radius=kernel_radius,
-      quiet=args.quiet,
-      img_names=config["training"]["img_names"],
-      shapes_to_skip_fitting=config["training"]["shapes_to_skip_fit"],
-      plot_freq=args.plot_freq,
-      plot_tag=f"case{tID}",
-    )
+    if strtobool(config["test-set"]["contrast"][0]) == True:
+      assam = RespiratoryReconstructSSAM(
+        shape=inputCoords,
+        xRay=edgePoints,
+        lmOrder=lmOrder,
+        normals=None,
+        transform=carinaArr[t] * 0.0,
+        img=copy(img),
+        # imgSpacing=spacing_xr,
+        imgCoords=imgCoords,
+        imgCoords_all=ssam.sam.imgCoords_all,
+        imgCoords_axes=config["training"]["img_axes"],
+        density=density,
+        model=modelDict,
+        modeNum=numModes,
+        c_edge=c_edge,
+        c_prior=c_prior,
+        c_dense=c_dense,
+        c_grad=c_grad,
+        c_anatomical=c_anatomical,
+        kernel_distance=kernel_distance,
+        kernel_radius=kernel_radius,
+        quiet=args.quiet,
+        img_names=config["training"]["img_names"],
+        shapes_to_skip_fitting=config["training"]["shapes_to_skip_fit"],
+        plot_freq=args.plot_freq,
+        plot_tag=f"case{target_id}",
+        rotation=config["training"]["rotation"],
+        contrast_outline=edgePoints_contrast,
+      )
+    else:
+      assam = RespiratoryReconstructSSAM(
+        shape=inputCoords,
+        xRay=edgePoints,
+        lmOrder=lmOrder,
+        normals=None,
+        transform=carinaArr[t] * 0.0,
+        img=copy(img),
+        # imgSpacing=spacing_xr,
+        imgCoords=imgCoords,
+        imgCoords_all=ssam.sam.imgCoords_all,
+        imgCoords_axes=config["training"]["img_axes"],
+        density=density,
+        model=modelDict,
+        modeNum=numModes,
+        c_edge=c_edge,
+        c_prior=c_prior,
+        c_dense=c_dense,
+        c_grad=c_grad,
+        c_anatomical=c_anatomical,
+        kernel_distance=kernel_distance,
+        kernel_radius=kernel_radius,
+        quiet=args.quiet,
+        img_names=config["training"]["img_names"],
+        shapes_to_skip_fitting=config["training"]["shapes_to_skip_fit"],
+        plot_freq=args.plot_freq,
+        plot_tag=f"case{target_id}",
+        rotation=config["training"]["rotation"],
+      )
     assam.spacing_xr = spacing_xr
     # import variables to class
     assam.variance = ssam.variance[:numModes]
@@ -1238,8 +1314,6 @@ if __name__ == "__main__":
               ],
             )
           plt.show()
-
-    exit()
     # initialise parameters to be optimised - including initial values + bounds
     optTrans_new = dict.fromkeys(["pose", "scale"])
     optTrans_new["pose"] = [0, 0]
@@ -1254,15 +1328,6 @@ if __name__ == "__main__":
         (-3, 3),
       ]
     )
-    # bounds = np.array(
-    #   [
-    #     (-0.0, 0.01),
-    #     (-0.0, 0.01),
-    #     # (0.7, 1.3),
-    #     (1.0, 1.01),  # (optTrans_new["scale"]*0.9, optTrans_new["scale"]*1.1),
-    #     (-3, 3),
-    #   ]
-    # )
 
     # initialise parameters that control optimisation process
     assam.optimiseStage = "both"  # tell class to optimise shape and pose
@@ -1323,7 +1388,7 @@ if __name__ == "__main__":
       delimiter=",",
     )
     """
-    out_airway_file = out_surf_file.format(tID, 'Airway', 'stl')
+    out_airway_file = out_surf_file.format(target_id, 'Airway', 'stl')
     morph_airway = MorphAirwayTemplateMesh(lm_template[lmOrder['Airway']], 
                                             outShape[lmOrder['Airway']], 
                                             mesh_template,
@@ -1338,7 +1403,7 @@ if __name__ == "__main__":
                                           outShape[lmOrder[lobe]], 
                                           template_lobe_mesh,
                                           quiet=True)
-      out_lobe_file = out_surf_file.format(tID, lobe, 'stl')
+      out_lobe_file = out_surf_file.format(target_id, lobe, 'stl')
       morph_lobe.mesh_target.write(out_lobe_file)
     """
     # if config['training']['num_imgs'] == 1:
@@ -1347,7 +1412,8 @@ if __name__ == "__main__":
     #   plt.scatter(outShape[:, axes[0]], outShape[:, axes[0]], s=2, c="black")
     #   plt.savefig("images/reconstruction/{}.png".format(tag), dpi=200)
     # else:
-    for i, axes in enumerate(config["training"]["img_axes"]):
+    # exit()
+    for i, (axes, angle) in enumerate(zip(config["training"]["img_axes"], config["training"]["rotation"])):
       extent = [
         imgCoords[:, axes[0]].min(),
         imgCoords[:, axes[0]].max(),
@@ -1355,20 +1421,25 @@ if __name__ == "__main__":
         imgCoords[:, axes[1]].max(),
       ]
       plt.close()
+      rot_shape = utils.rotate_coords_about_z(outShape, angle)
       plt.imshow(img.reshape(-1, 500, 500)[i], cmap="gray", extent=extent)
-      plt.scatter(outShape[:, axes[0]], outShape[:, axes[1]], s=2, c="yellow")
+      plt.scatter(rot_shape[:, axes[0]], rot_shape[:, axes[1]], s=2, c="yellow")
       plt.savefig(f"images/reconstruction/{tag}-view{i}.png", dpi=200)
 
     # shape parameters for ground truth
     b_gt = getShapeParameters(
-      inputCoords["ALL"], tLM, assam.model_s["ALL"], assam.std
+      inputCoords["ALL"], target_lm, assam.model_s["ALL"], assam.std
     )
     shape_parameter_diff = b_gt - optAll["b"]
     print("parameter difference is")
     print(shape_parameter_diff)
 
-    distX = utils.euclideanDist(outShape[:, [0]], lmProj_test[0][:, [0]])
-    dist2D = utils.euclideanDist(outShape[:, [0, 2]], lmProj_test[0][:, [0, 2]])
+    distX = utils.euclideanDist(
+      outShape[:, [0]], ground_truth_landmarks[0][:, [0]]
+    )
+    dist2D = utils.euclideanDist(
+      outShape[:, [0, 2]], ground_truth_landmarks[0][:, [0, 2]]
+    )
 
     # plotDensityError(outShape, density_at_lm,
     #                     tag='from1')
@@ -1382,13 +1453,22 @@ if __name__ == "__main__":
       assam.model_g["ALL"][: len(optAll["b"])],
       optAll["b"] * np.sqrt(assam.variance),
     ).reshape(-1, config["training"]["num_imgs"])
+    outShape_base = copy(outShape)
+    ground_truth_landmarks_base = copy(ground_truth_landmarks[t])
     for i in range(0, config["training"]["num_imgs"]):
       # plot
+      # if config["training"]["rotation"][i] == 0:
+      #   pass
+      # else:
+      #   outShape = utils.rotate_coords_about_z(outShape, 45)
+
       density_error_for_point_cloud(
-        outShape,
+        utils.rotate_coords_about_z(
+          outShape, config["training"]["rotation"][i]
+        ),
         imgCoords,
         # img[i],
-        tImg.reshape(-1, 500, 500)[i],
+        target_img.reshape(-1, 500, 500)[i],
         density_from_model[:, i],
         tag=f"reconstructionFromModel_view{i}",
         axes=config["training"]["img_axes"][i],
@@ -1399,19 +1479,25 @@ if __name__ == "__main__":
       test_carina = nodalCoordsOrig[test_index[t], 1]
 
       density_error_for_point_cloud(
-        lmProj_test[t],
+        utils.rotate_coords_about_z(
+          ground_truth_landmarks[t], config["training"]["rotation"][i]
+        ),
         imgCoords,
         # img[i],
-        tImg.reshape(-1, 500, 500)[i],
+        target_img.reshape(-1, 500, 500)[i],
         density_from_model[:, i],
         tag=f"groundTruthFromModel_view{i}",
         axes=config["training"]["img_axes"][i],
       )
 
       density_comparison(
-        outShape,
-        lmProj_test[t],  # +testOrigin[t],
-        tImg.reshape(-1, 500, 500)[i],
+        utils.rotate_coords_about_z(
+          outShape, config["training"]["rotation"][i]
+        ),
+        utils.rotate_coords_about_z(
+          ground_truth_landmarks[t], config["training"]["rotation"][i]
+        ),
+        target_img.reshape(-1, 500, 500)[i],
         imgCoords,
         imgCoords,
         tag=f"_view{i}",

respiratoryReconstructionSSAM.py
@@ -71,7 +71,9 @@ class RespiratoryReconstructSSAM:
     shapes_to_skip_fitting=["None"],
     plot_freq=250,
     plot_tag="",
-  ):  # 7):
+    rotation=[0],
+    **kwargs,
+  ): 
 
     # tunable hyper-parameters
     self.c_edge = c_edge
@@ -86,7 +88,7 @@ class RespiratoryReconstructSSAM:
     self.img_names = img_names
     self.shapes_to_skip_fitting = shapes_to_skip_fitting
     self.plot_freq = plot_freq  # frequency to show debug plots
-    self.plot_tag = plot_tag # file name for debug plots
+    self.plot_tag = plot_tag  # file name for debug plots
 
     self.lobes = ["RUL", "RML", "RLL", "LUL", "LLL"]
 
@@ -105,6 +107,7 @@ class RespiratoryReconstructSSAM:
 
     # number of images used for reconstruction (assumes img shape = 500 x 500)
     self.number_of_imgs = max(np.sum(img.shape) - 1000, 1)
+    self.rotation = rotation
 
     """
     # self.img = ssam.sam.normaliseTestImageDensity(img)
@@ -116,12 +119,13 @@ class RespiratoryReconstructSSAM:
     """
     # format x-ray for image enhancement
     self.img = copy(img)  # -XR image array
-    img_scaled = self.rescaleProjection(img)
+    img_scaled = self.rescaleProjection(copy(img))
     img_scaled *= 0.999  # avoid floating point error in scalar causing img > 1
+    self.img_scaled = copy(img_scaled)
     # img_filt = utils.bilateralfilter(img, 10)
     # if img_filt.max()>1:
     #   img_filt = img_filt/img_filt.max()
-    img_local = utils.localNormalisation(img_scaled, 20)
+    img_local = utils.localNormalisation(copy(img_scaled), 20)
     img_local = self.rescaleProjection(img_local)
     self.img_local = np.round(img_local, 4)
     print(self.img_local.min(), self.img_local.max())
@@ -188,6 +192,13 @@ class RespiratoryReconstructSSAM:
         self.scaleShape(shape), self.density.mean(axis=0)
       )
 
+    if "contrast_outline" in kwargs.keys():
+      print("outline found for contrast X-ray")
+      self.contrast_outline_found = True
+      self.contrast_outline = kwargs["contrast_outline"]
+    else:
+      self.contrast_outline_found = False
+
   def rescaleProjection(self, img):
     """
     Set pixel value to [0:1] for a single or multiple projections.
@@ -224,7 +235,6 @@ class RespiratoryReconstructSSAM:
     # pose = dict.fromkeys(shape.keys())
     # scale = dict.fromkeys(shape.keys())
 
-    print("\nNext {0} loop iter {1}".format(self.optimiseStage, self.optIter))
 
     prior = []
     # -copy mean shape before shape is adjusted
@@ -233,9 +243,10 @@ class RespiratoryReconstructSSAM:
     # -call test parameters from optimizer
     self.b = copy(b)
 
-    # if not self.quiet:
-    print("\t\t opt params ", pose, scale)
-    print("\t\t\t", b)
+    if not self.quiet:
+      print("\nNext {0} loop iter {1}".format(self.optimiseStage, self.optIter))
+      print("\t\t opt params ", pose, scale)
+      print("\t\t\t", b)
     # -apply new shape parameters to each lobe
     all_morphed = self.morphAirway(
       meanShape,  # shape[key],
@@ -246,6 +257,7 @@ class RespiratoryReconstructSSAM:
     # when registering initial shape, overwrite morphed shape with mean shape
     if self.optimiseStage in ["pose", "align"]:
       all_morphed = copy(meanShape)
+      print("aligning mean shape -- not applying morph")
 
     """ """
     if pose.size == 2:
@@ -257,10 +269,10 @@ class RespiratoryReconstructSSAM:
     all_morphed = all_morphed + pose
     """ """
 
-    print('pose and scale off')
+    # print("pose and scale off")
     airway_morphed = all_morphed[self.lmOrder["Airway"]]
     # check shape has not moved to be larger than XR or located outside XR
-    # TODO - will not work on non-DRR dataset i.e real XR as true imgCoords 
+    # TODO - will not work on non-DRR dataset i.e real XR as true imgCoords
     #        are unknown in three dimensions. Could use minMax of edge map?
     outside_bounds = np.any(
       (all_morphed[:, 0].max() > self.imgCoords[:, 0].max())
@@ -276,26 +288,58 @@ class RespiratoryReconstructSSAM:
       lobe_morphed[lobe] = all_morphed[self.lmOrder[lobe]]
     # get losses
     fit = self.fitTerm(xRay, lobe_morphed, shapenorms)
-    loss_anatomicalShadow = 0.
+    if self.contrast_outline_found:
+      fit += self.contrast_fit(self.contrast_outline, airway_morphed)*0.5
+    loss_anatomicalShadow = 0.0
     if self.number_of_imgs > 1:
       density_t = [None] * self.number_of_imgs
       for i, axes in enumerate(self.imgCoords_axes):
+        if self.rotation[i] == 0:
+          all_morphed_rot = copy(all_morphed)
+          airway_morphed_rot = copy(airway_morphed)
+        elif self.rotation[i] == 45:
+          all_morphed_rot = utils.rotate_coords_about_z(all_morphed, 45)
+          airway_morphed_rot = utils.rotate_coords_about_z(airway_morphed, 45)
+        else:
+          all_morphed_rot = utils.rotate_coords_about_z(all_morphed, self.rotation[i])
+          airway_morphed_rot = utils.rotate_coords_about_z(airway_morphed, self.rotation[i])
         density_t[i] = self.getDensity(
-          all_morphed, self.img[i], self.imgCoords, axes
+          all_morphed_rot, self.img[i], self.imgCoords, axes
         )
         # can turn off anatomical shadow by setting coeff to 0
         # coeff/2 as we want to keep weighting same relative to other losses
         if self.c_anatomical != 0.0:
-          loss_anatomicalShadow += self.c_anatomical/2 * self.anatomicalShadow(
-            self.img_local,
-            self.imgCoords[:, axes],
-            airway_morphed,
-            self.lmOrder,
-            self.projLM_ID_multipleproj[i]["Airway"],
-            kernel_distance=self.kernel_distance,
-            kernel_radius=self.kernel_radius,
-            axes=axes,
-          )
+          # loss_anatomicalShadow += (
+          #   self.c_anatomical
+          #   / self.number_of_imgs
+          #   * self.anatomicalShadow(
+          #       self.img_local[i],
+          #       self.imgCoords[:, axes],
+          #       airway_morphed_rot,
+          #       self.lmOrder,
+          #       self.projLM_ID_multipleproj[i]["Airway"],
+          #       kernel_distance=self.kernel_distance,
+          #       kernel_radius=self.kernel_radius,
+          #       axes=axes,
+          #       debug_fname=f"iter{self.optIter}proj{i}shadow.png",
+          #   )
+          # )
+          if i == 0:
+            loss_anatomicalShadow += (
+              self.c_anatomical
+              / 1.0
+              * self.anatomicalShadow(
+                  self.img_local[i],
+                  self.imgCoords[:, axes],
+                  airway_morphed_rot,
+                  self.lmOrder,
+                  self.projLM_ID_multipleproj[i]["Airway"],
+                  kernel_distance=self.kernel_distance,
+                  kernel_radius=self.kernel_radius,
+                  axes=axes,
+                  debug_fname=f"iter{self.optIter}proj{i}shadow.png",
+              )
+            )
       # convert list to array
       # change from shape N_imgs, N_lms -> N_lms, N_imgs
       # density_t = np.array(density_t).T
@@ -315,6 +359,7 @@ class RespiratoryReconstructSSAM:
           kernel_distance=self.kernel_distance,
           kernel_radius=self.kernel_radius,
           axes=self.imgCoords_axes[0],
+          debug_fname=f"iter{self.optIter}shadow.png",
         )
 
     # change so density at landmark location is scaled same as modelled density
@@ -328,14 +373,29 @@ class RespiratoryReconstructSSAM:
     prior = self.priorTerm(shapeIn, self.meanScaled)
 
     # TODO - FILTER DENSITY LOSS TO ONLY VOXELS WITHIN imgCoords
+    """
+    densityFit_airway = self.densityLoss(
+      density_t.reshape(-1)[:len(self.shape["Airway"])],
+      self.density.mean(axis=0).reshape(-1)[:len(self.shape["Airway"])],
+      self.model_g["Airway"][: len(self.b)],
+      self.b,
+    )
+    densityFit_lobes = self.densityLoss(
+      density_t.reshape(-1)[len(self.shape["Airway"]):],
+      self.density.mean(axis=0).reshape(-1)[len(self.shape["Airway"]):],
+      self.model_g["ALL"][: len(self.b)][:, len(self.shape["Airway"]):],
+      self.b,
+    )
+    """
     densityFit = self.densityLoss(
       density_t.reshape(-1),
       self.density.mean(axis=0).reshape(-1),
       self.model_g["ALL"][: len(self.b)],
       self.b,
     )
-    if not self.quiet: 
-      print("\tfit loss",fit)
+    # densityFit = densityFit_lobes + densityFit_airway*10.
+    if not self.quiet:
+      print("\tfit loss", fit)
       print("\tdensity loss", densityFit)
       print("\tprior loss", prior)  # round(prior,4))
     tallest_pt = airway_morphed[np.argmax(airway_morphed[:, 2])]
@@ -350,23 +410,24 @@ class RespiratoryReconstructSSAM:
     )
     # E += top_dist * 1.0
     E += loss_anatomicalShadow
-    print("top dist", top_dist)
+    # print("top dist", top_dist)
 
     if outside_bounds and self.optIter > 100:
       # as we use max value from loss log, make sure this is only after 100
       #   iterations, so the max value will not be sensitive to outliers
-      E += max(self.lossLog)*0.2
+      E += max(self.lossLog) * 0.2
       if not self.quiet:
         print("OUTSIDE OF BOUNDS")
-    print("\ttotal loss", E)
+    if (self.optIter % 1000) == 0:
+      print(f"\t{self.optimiseStage} iter is {self.optIter}. total loss {E}")
 
     # return debugging plot of outline overlaid on image
-    if self.optIter % self.plot_freq == 0: #and not self.quiet:
+    if (self.optIter % self.plot_freq == 0) and not self.quiet:
       if self.number_of_imgs == 1:
         self.overlayAirwayOnXR(self.img, all_morphed, scale, pose)
       elif self.number_of_imgs >= 2:
         self.overlayAirwayOnXR_multipleimgs(self.img, all_morphed, scale, pose)
-    assert ~np.isnan(densityFit), 'unexpected NaN {}'.format(density_t)
+    assert ~np.isnan(densityFit), "unexpected NaN {}".format(density_t)
     return E
 
   def gradientTerm(self, coords, imgGrad, imgCoords):
@@ -382,7 +443,16 @@ class RespiratoryReconstructSSAM:
     return (-1.0 * lmGrad).mean()
 
   def anatomicalShadow(
-    self, img, img_coords, landmarks, lmOrder, projLM_ID, kernel_distance, kernel_radius, axes=[0, 2]
+    self,
+    img,
+    img_coords,
+    landmarks,
+    lmOrder,
+    projLM_ID,
+    kernel_distance,
+    kernel_radius,
+    axes=[0, 2],
+    debug_fname="shadow.png"
   ):
     """
     anatomical shadow function proposed by
@@ -407,10 +477,10 @@ class RespiratoryReconstructSSAM:
     """
 
     extent = [
-      -self.img.shape[1] / 2.0 * self.spacing_xr[0],
-      self.img.shape[1] / 2.0 * self.spacing_xr[0],
-      -self.img.shape[0] / 2.0 * self.spacing_xr[2],
-      self.img.shape[0] / 2.0 * self.spacing_xr[2],
+      -img.shape[1] / 2.0 * self.spacing_xr[0],
+      img.shape[1] / 2.0 * self.spacing_xr[0],
+      -img.shape[0] / 2.0 * self.spacing_xr[2],
+      img.shape[0] / 2.0 * self.spacing_xr[2],
     ]
 
     skeleton_ids = lmOrder["SKELETON"]
@@ -418,9 +488,7 @@ class RespiratoryReconstructSSAM:
     # get airway points not on skeleton (surface only)
     airway_surf_ids = airway_ids[~np.isin(airway_ids, skeleton_ids)]
     # get surface points that are in projected points list
-    airway_surf_ids = airway_surf_ids[
-      np.isin(airway_surf_ids, projLM_ID)
-    ]
+    airway_surf_ids = airway_surf_ids[np.isin(airway_surf_ids, projLM_ID)]
 
     skel_pts = landmarks[skeleton_ids][:, axes]
     silhouette_pts = landmarks[airway_surf_ids][:, axes]
@@ -508,15 +576,15 @@ class RespiratoryReconstructSSAM:
     energy = np.array(energy)
     silhouette_pts = np.delete(silhouette_pts, delInd, axis=0)
 
-    # if self.optIter % 500 == 0 and not self.quiet:
-    #   plt.close()
-    #   # for debugging anatomical shadow values
-    #   fig, ax = plt.subplots()
-    #   ax.imshow(img, cmap='gray', extent=extent)
-    #   scatter = ax.scatter(silhouette_pts[:,0], silhouette_pts[:,1],
-    #                         c=energy, s=2)
-    #   plt.colorbar(scatter)
-    #   plt.savefig('images/reconstruction/debug/iter{}shadow.png'.format(self.optIter))
+    if self.optIter % 500 == 0 and not self.quiet:
+      plt.close()
+      # for debugging anatomical shadow values
+      fig, ax = plt.subplots()
+      ax.imshow(img, cmap='gray', extent=extent)
+      scatter = ax.scatter(silhouette_pts[:,0], silhouette_pts[:,1],
+                            c=energy, s=2)
+      plt.colorbar(scatter)
+      plt.savefig(f'images/reconstruction/debug/{debug_fname}')
     # '''
     # # plt.show()
     # # exit()
@@ -527,7 +595,8 @@ class RespiratoryReconstructSSAM:
     if len(energy) == 0:
       return 0
     else:
-      if not self.quiet: print("\tanatomicalShadow", energy.sum(), energy.mean())
+      if not self.quiet:
+        print("\tanatomicalShadow", energy.sum(), energy.mean())
       return (energy).mean()
 
   def scaleShape(self, shape):
@@ -745,9 +814,9 @@ class RespiratoryReconstructSSAM:
 
     tag = ""
     utils.plotLoss(
-      self.lossLog, 
-      tag=self.optimiseStage+self.plot_tag, 
-      wdir="images/reconstruction/"
+      self.lossLog,
+      tag=self.optimiseStage + self.plot_tag,
+      wdir="images/reconstruction/",
     )  # -plot loss
 
     optOut = dict.fromkeys(["pose", "scale", "b"])
@@ -766,9 +835,17 @@ class RespiratoryReconstructSSAM:
     fit_list = []  # * len(shapeDict.keys())
     n_proj = self.number_of_imgs
     for img_index, axes in zip(range(0, n_proj), self.imgCoords_axes):
+      if self.rotation[img_index] == 45: continue
       if n_proj == 1:
         projLM_ID_i = self.projLM_ID.copy()
-        xRay_i = xRay[0].copy()
+        if type(xRay) == list:
+          # below line of code only needed if two outlines given by accident in json
+          xRay_i = xRay[0].copy()
+        elif type(xRay) == np.ndarray:
+          xRay_i = xRay.copy()
+        else:
+          print(f"unrecognised variable type for xRay in fitTerm, {type(xRay)}. Exiting.")
+          exit()
       else:
         projLM_ID_i = self.projLM_ID_multipleproj[img_index]
         xRay_i = xRay[img_index]
@@ -776,13 +853,23 @@ class RespiratoryReconstructSSAM:
         # skip certain lobes or sub-shapes for different images
         # as they may be non-visible
         if key not in self.shapes_to_skip_fitting[img_index]:
-          shape = shapeDict[key][projLM_ID_i[key]][:, axes]
+          shape = utils.rotate_coords_about_z(shapeDict[key], self.rotation[img_index])[projLM_ID_i[key]][:, axes]
           num_points += len(shape)
 
           fit_list.append(self.fitLoss(shape, xRay_i, scaler))
+
     E_fit = (1 / (num_points * n_proj)) * np.sum(fit_list)
 
     return E_fit
+
+  def contrast_fit(self, xRay, shape, scaler=5.0):
+    """
+    Compute difference between outlines of X-R and shape if X-R with contrast is
+    given in kwargs.
+    """
+    fit = self.fitLoss(shape[:, [0, 2]], xRay, scaler) / len(shape)
+    return fit
+
 
   def normalisedDistance(self, shape1, shape2, scaler=5):
     """
@@ -819,15 +906,22 @@ class RespiratoryReconstructSSAM:
     return E_prior
 
   def overlayAirwayOnXR(self, img, coords, scale, pos, tag=""):
-    extent = [self.imgCoords[:,self.imgCoords_axes[0][0]].min(),
-              self.imgCoords[:,self.imgCoords_axes[0][0]].max(),
-              self.imgCoords[:,self.imgCoords_axes[0][1]].min(),
-              self.imgCoords[:,self.imgCoords_axes[0][1]].max()]
+    extent = [
+      self.imgCoords[:, self.imgCoords_axes[0][0]].min(),
+      self.imgCoords[:, self.imgCoords_axes[0][0]].max(),
+      self.imgCoords[:, self.imgCoords_axes[0][1]].min(),
+      self.imgCoords[:, self.imgCoords_axes[0][1]].max(),
+    ]
     plt.close()
     plt.imshow(img, cmap="gray", extent=extent)
-    plt.scatter(
-      self.xRay[0][:, 0], self.xRay[0][:, 1], s=4, c="black"
-    )  # , alpha=0.2)
+    if type(self.xRay) == list:
+      # below line of code only needed if two outlines given by accident in json
+      xRay_i = self.xRay[0].copy()
+    elif type(self.xRay) == np.ndarray:
+      xRay_i = self.xRay.copy()
+    plt.scatter(xRay_i[:, 0], xRay_i[:, 1], s=4, c="black")
+    if self.contrast_outline_found:
+      plt.scatter(self.contrast_outline[:, 0], self.contrast_outline[:, 1], s=4, c="pink")
     # plt.scatter(coords[self.projLM_IDAll,0],
     #             coords[self.projLM_IDAll,2],
     #             s=2, c='yellow')
@@ -865,22 +959,24 @@ class RespiratoryReconstructSSAM:
     return None
 
   def overlayAirwayOnXR_multipleimgs(self, img, coords, scale, pos, tag=""):
-    for i, (im_i, axes) in enumerate(zip(img, self.imgCoords_axes)):
-      extent = [self.imgCoords[:,axes[0]].min(),
-                self.imgCoords[:,axes[0]].max(),
-                self.imgCoords[:,axes[1]].min(),
-                self.imgCoords[:,axes[1]].max()]
+    for i, (im_i, axes, angle) in enumerate(zip(img, self.imgCoords_axes, self.rotation)):
+      extent = [
+        self.imgCoords[:, axes[0]].min(),
+        self.imgCoords[:, axes[0]].max(),
+        self.imgCoords[:, axes[1]].min(),
+        self.imgCoords[:, axes[1]].max(),
+      ]
       plt.close()
       plt.imshow(im_i, cmap="gray", extent=extent)
-      plt.scatter(self.xRay[i][:, 0], self.xRay[i][:, 1], s=4, c="black")
+      if self.rotation[i] != 45: 
+        plt.scatter(self.xRay[i][:, 0], self.xRay[i][:, 1], s=4, c="black")
+      coords_to_plot = utils.rotate_coords_about_z(coords, angle)
       for key in self.projLM_ID.keys():
         projLM_index = self.projLM_ID_multipleproj[i][key]
         if key not in self.shapes_to_skip_fitting[i]:
           plt.scatter(
-            # coords[self.lmOrder[key]][projLM_key, 0],
-            # coords[self.lmOrder[key]][projLM_key, 2],
-            coords[self.lmOrder[key]][projLM_index, axes[0]],
-            coords[self.lmOrder[key]][projLM_index, axes[1]],
+            coords_to_plot[self.lmOrder[key]][projLM_index, axes[0]],
+            coords_to_plot[self.lmOrder[key]][projLM_index, axes[1]],
             s=8,
             c="yellow",
           )
@@ -897,7 +993,6 @@ class RespiratoryReconstructSSAM:
         bbox_inches="tight",
         pad_inches=0,
       )
-    # exit()
     return None
 
   def getProjectionLandmarks(self, faceIDs, faceNorms, points, plane=1):

respiratorySSAM.py
@@ -55,6 +55,7 @@ class RespiratorySSAM:
     imgsOrigin,
     imgsSpacing,
     train_size=0.9,
+    rotation=[0],
   ):
     # check all input datasets have same number of samples
     assert (
@@ -89,12 +90,37 @@ class RespiratorySSAM:
       self.density = self.sam.density
     elif imgs.ndim == 4:
       self.sam = RespiratorySAM(lm_ct, imgs[:, 0], imgsOrigin, imgsSpacing)
-      self.sam_left = RespiratorySAM(
-        lm_ct, imgs[:, 1], imgsOrigin, imgsSpacing, axes=[1, 2]
-      )
-      self.density = np.stack(
-        (self.sam.density, self.sam_left.density), axis=-1
-      )
+      # load extra SAM for each DRR projection
+      num_extra_proj = (imgs.shape[1] - 1)
+      sam_extra_proj = [None]*(num_extra_proj)
+      density_extra_proj = [None]*(num_extra_proj)
+      reshape_to_shape = (self.sam.density.shape[0], self.sam.density.shape[1], 1)
+      self.density = self.sam.density.copy().reshape(reshape_to_shape)
+      for extra_proj_i in range(0, num_extra_proj):
+        if rotation[extra_proj_i+1] == 0:
+          sam_extra_proj[extra_proj_i] = RespiratorySAM(
+            lm_ct, imgs[:, extra_proj_i+1], imgsOrigin, imgsSpacing, axes=[1, 2]
+          )
+        else:
+          # print()
+          rot_coords = []
+          for patient in lm_ct:
+            rot_coords_i = utils.rotate_coords_about_z(patient, rotation[extra_proj_i+1])
+            rot_coords.append(rot_coords_i)
+          # rot_coords = utils.rotate_coords_about_z(lm_ct, rotation[extra_proj_i+1])
+          # print(rot_coords)
+          rot_coords = np.array(rot_coords)
+          sam_extra_proj[extra_proj_i] = RespiratorySAM(
+            rot_coords, 
+            imgs[:, extra_proj_i+1], 
+            imgsOrigin, 
+            imgsSpacing, 
+            axes=[0, 2],
+          )
+        density_extra_proj[extra_proj_i] = copy(sam_extra_proj[extra_proj_i].density).reshape(reshape_to_shape)
+        self.density = np.dstack(
+          (self.density, density_extra_proj[extra_proj_i])
+        )
 
     # -initialise appearance model data
     self.imgs = imgs