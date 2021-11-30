e450f6bbca38dc10680af1b75438feb30df892b8
jvwilliams23 <jw144@hw.ac.uk>
2021-11-30 20:06:36 +0000

    code to test with no lobe fissures

---
+++ lung_volume_error_no_fissures.py
@@ -0,0 +1,141 @@
+import numpy as np
+import pandas as pd
+import vedo as v
+import matplotlib.pyplot as plt
+from sys import exit
+from glob import glob
+from copy import copy
+import argparse
+from datetime import date
+
+
+today = str(date.today())  # "2020-08-03"
+units = "pct"
+unitsList = ["pct", "mm"]  # ["pct"] #["pct", "mm"]
+debug = False
+
+lobeCol = [
+  (0.47843137254901963, 0.47843137254901963, 0.47843137254901963),
+  (1.0, 170.0 / 255.0, 0.0),
+  (1.0, 0.3333333333333333, 0.4980392156862745),
+  (0.0, 0.6666666666666666, 1.0),
+  (100.0 / 255.0, 220 / 255.0, 184.0 / 255.0),
+]
+
+# lungCol = ['blue', 'red']
+lungCol = [lobeCol[1], lobeCol[-2]]
+
+lobeIDs = {
+  "RUL": "4",
+  "RML": "5",
+  "RLL": "6",
+  "LUL": "7",
+  "LLL": "8",
+}
+lobes = ["RUL", "RML", "RLL", "LUL", "LLL"]
+# = ["blue","black", "green"]
+lobeLine = ["-", "--", "-.", "-", "-."]
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('--caseID', '-c',
+                    default='3948', 
+                    type=str,#, required=True,
+                    help='training data case'
+                    )
+parser.add_argument('--filename', '-f',
+                    default='reconstruction', 
+                    type=str,
+                    help='string for filename [default is reconstruction_case]'
+                    )
+parser.add_argument('--out_dir', '-o',
+                    default="morphologicalAnalysis/", 
+                    type=str,
+                    help='directory to find output volumes'
+                    )
+parser.add_argument('--gt_dir', '-g',
+                    default="/home/josh/3DSlicer/project/luna16Rescaled/",
+                    type=str,
+                    help='directory to find ground truth surfaces'
+                    )
+args = parser.parse_args()
+
+
+def get_vol(pred, gt):
+  v_p = pred.volume()
+  v_gt = gt.volume()
+  return v_p, v_gt
+
+
+def get_vol_error(v_p, v_gt):
+  # print("predicted volume is", v_p)
+  # print("gt volume is", v_gt)
+  # print(abs(v_p-v_gt)/max(v_gt,v_p)*100,"%\n")
+  return (v_p - v_gt) / (v_gt) * 100
+
+
+print("Reading data")
+surfName = "{}_case{}_{}.stl"
+
+volKeys = lobes + ["Right"] + ["Left"] + ["Total"]
+vol_error_keys = ["Right", "Left", "Total"]
+
+gt_dir = f"{args.gt_dir}/case{args.caseID}/"
+gt_name = args.caseID + "_mm_"  # "seg-case20-"
+vol_error = dict.fromkeys(volKeys)
+vol_pred = dict.fromkeys(volKeys)
+vol_gt = dict.fromkeys(volKeys)
+
+vol_pred["Right"] = 0.0
+vol_pred["Left"] = 0.0
+vol_pred["Total"] = 0.0
+vol_gt["Right"] = 0.0
+vol_gt["Left"] = 0.0
+vol_gt["Total"] = 0.0
+
+for key in volKeys:
+  if key in lobes:
+    surfDir = "surfaces/"
+    vol_gt[key] = v.load(gt_dir + gt_name + lobeIDs[key] + ".stl").volume()
+    vol_pred[key] = 0
+    vol_error[key] = np.inf
+
+    if key[0] == "R":
+      vol_gt["Right"] += vol_gt[key]
+    elif key[0] == "L":
+      vol_gt["Left"] += vol_gt[key]
+    vol_gt["Total"] += vol_gt[key]
+
+vol_pred["Left"] = v.load(surfDir + surfName.format(args.filename, args.caseID, "left")).volume()
+vol_pred["Right"] = v.load(surfDir + surfName.format(args.filename, args.caseID, "right")).volume()
+vol_pred["Total"] = vol_pred["Left"] + vol_pred["Right"]
+for key in vol_error_keys:
+  vol_error[key] = (vol_pred[key] - vol_gt[key]) / vol_gt[key] * 100.
+
+# write data like [shape, ground truth, reconstructon, diff [mm^3], diff [%]]
+shape_names = list(vol_error.keys())
+vol_pred_arr = np.round(np.array(list(vol_pred.values())), 2)
+vol_gt_arr = np.round(np.array(list(vol_gt.values())), 2)
+vol_error_arr = np.round(np.array(list(vol_error.values())), 2)
+data_to_write = np.c_[
+  shape_names,
+  vol_gt_arr,
+  vol_pred_arr,
+  vol_pred_arr - vol_gt_arr,
+  vol_error_arr,
+]
+print("error is ")
+print(vol_error_arr)
+
+pd.DataFrame(data_to_write).to_csv(
+  f"{args.out_dir}/lobeVolumeStats{args.caseID}_{args.filename}.txt",
+  sep="\t",
+  index=False,
+  header=[
+    "shape",
+    "ground truth [mm^3]",
+    "reconstruction [mm^3]",
+    "difference [mm^3]",
+    "difference [%]",
+  ],
+  float_format='%g'
+)
---
+++ post_morph_mesh_no_lobes.py
@@ -0,0 +1,124 @@
+"""
+Post-processing script for morphing template mesh to fit pre-computed landmarks
+"""
+
+import argparse
+import numpy as np 
+from glob import glob
+from sys import exit
+import vedo as v
+
+from morphAirwayTemplateMesh import MorphAirwayTemplateMesh
+from morphTemplateMesh import MorphTemplateMesh as MorphLobarTemplateMesh
+from respiratoryReconstructionSSAM import RespiratoryReconstructSSAM
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument('--caseID', '-c',
+                    default='3948', 
+                    type=str,#, required=True,
+                    help='training data case'
+                    )
+parser.add_argument('--filename', '-f',
+                    default='reconstruction', 
+                    type=str,
+                    help='string for filename [default is reconstruction_case]'
+                    )
+parser.add_argument('--landmark_dir', '-ld',
+                    default="allLandmarks/", 
+                    type=str,
+                    help='directory to find ground truth landmarks'
+                    )
+parser.add_argument('--out_lm_dir', '-o',
+                    default="outputLandmarks/", 
+                    type=str,
+                    help='directory to find output landmarks from SSAM'
+                    )
+args = parser.parse_args()
+
+
+if __name__ == "__main__":
+  case = args.caseID
+  out_lm_dir = args.out_lm_dir
+  landmark_dir = args.landmark_dir
+  print("reading from ", landmark_dir)
+  surf_dir = "surfaces/"
+  # hard-code templates landmarks. 
+  # this is one that appears to adapt best to other cases
+  template_dir = "templates/coarserTemplates/"
+  # template_lmFile = f"{landmark_dir}/allLandmarks3948.csv"
+  # template_airway_file = (
+  #   "segmentations/template3948/smooth_newtemplate3948_mm.stl"
+  # )
+  # strings to iterate through for dictionaries of various shapes
+  shapes = ["Airway", "RUL", "RML", "RLL", "LUL", "LLL"]
+  lobes = ["RUL", "RML", "RLL", "LUL", "LLL"]
+  # numbering for each lobe in file
+  lNums = {"RUL": "4", "RML": "5", "RLL": "6", "LUL": "7", "LLL": "8"}
+
+  # find which set of landmarks correspond to each part of lungs
+  landmark_ordering = dict.fromkeys(shapes)
+  landmark_ordering["SKELETON"] = np.loadtxt(
+    landmark_dir + "landmarkIndexSkeleton.txt"
+  ).astype(int)
+  landmark_ordering["LUNGS"] = []
+  landmark_ordering["right"] = []
+  landmark_ordering["left"] = []
+  for shape in shapes:
+    landmark_ordering[shape] = np.loadtxt(
+      landmark_dir + f"landmarkIndex{shape}.txt", dtype=int
+    )
+    if shape[0].upper() == "R":
+      landmark_ordering["right"].append(landmark_ordering[shape])
+    if shape[0].upper() == "L":
+      landmark_ordering["left"].append(landmark_ordering[shape])
+  landmark_ordering["right"] = np.hstack(landmark_ordering["right"])
+  landmark_ordering["left"] = np.hstack(landmark_ordering["left"])
+
+  # file name for target landmarks to morph mesh to, and name of mesh to write
+  target_lm_file_name = f"{args.filename}_case{case}"
+  target_lm_file = f"{out_lm_dir}/{target_lm_file_name}_ALL.csv"
+  target_surf_name = f"{surf_dir}/{target_lm_file_name}_airway.stl"
+  # get carina to align mesh and landmarks to same coord system 
+  template_lmFileOrig = "landmarks/manual-jw/landmarks3948.csv"
+  carinaTemplate = (
+    np.loadtxt(
+      template_lmFileOrig, skiprows=1, delimiter=",", usecols=[1, 2, 3]
+    )[1]
+    * -1
+  )
+
+  # load mesh and landmarks for template and target
+  # lm_template = np.loadtxt(
+  #   template_lmFile, skiprows=1, delimiter=",", usecols=[0, 1, 2]
+  # )
+  # template_airway_mesh = v.load(template_airway_file)
+  # template_airway_mesh = template_airway_mesh.pos(carinaTemplate)
+  target_lms = np.loadtxt(target_lm_file, delimiter=",", skiprows=1)
+
+  # morph_airway = MorphAirwayTemplateMesh(
+  #   lm_template[landmark_ordering["Airway"]],
+  #   target_lms[landmark_ordering["Airway"]],
+  #   template_airway_mesh,
+  #   sigma=0.3,
+  #   quiet=False,
+  # )
+  # morph_airway.mesh_target.write(target_surf_name)
+
+  for lung in ["right", "left"]:
+    print(f"making {lung} template mesh")
+    # load data for morphing
+    template_lmFile = f"{landmark_dir}/allLandmarks8684.csv"
+    lm_template = np.loadtxt(
+      template_lmFile, skiprows=1, delimiter=",", usecols=[0, 1, 2]
+    )
+    template_mesh_file_lung = f"templates/8684_mm_{lung}.stl"
+    template_lung_mesh = v.load(template_mesh_file_lung)
+    morph_lung = MorphLobarTemplateMesh(
+      lm_template[landmark_ordering[lung]],
+      target_lms[landmark_ordering[lung]],
+      template_lung_mesh,
+      sigma=0.3,
+      quiet=False,
+    )
+    target_lung_surf_out = f"{surf_dir}/{target_lm_file_name}_{lung}.stl"
+    morph_lung.mesh_target.write(target_lung_surf_out)
